4899f9fb9faab54e8b0c257d4f7dde32f33d0d5c
Dmitriy_Malakhov <lesnichiy1986@gmail.com>
2022-12-25 16:45:39 +0300

    program is written Тask01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] array = new string[5] { "hello", "2", "world", ":-)", "bye" };
+
+DecreaseArray(array);
+
+void DecreaseArray(string[] arr)
+{
+    
+    int temp = 0;
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arr[temp] = arr[i];
+            temp++;
+
+            Console.Write($"{arr[i]},");
+        }
+    }
+    Console.Write("]");
+}
+
+
